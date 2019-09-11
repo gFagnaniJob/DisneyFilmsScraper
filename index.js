9de3d17e739c2a8aaeed7f7d6d2034745785be66
Giuseppe Fagnani <g.fagnani@reply.it>
2019-09-11 15:34:13 +0200

    aggiunto csv alternativo

--- index.js
+++ index.js
@@ -23,9 +23,27 @@ filmParser()
             personaggi = [];
         }
 
-        console.log(films);
-        const transformed = new csv(films);
-        return transformed.toDisk("./disneyData.csv");
+        var listaFilm = [];
+
+        for (var i=0; i<data.length; i++) {
+            var listaPersonaggi = [];
+            for (var j=0; j<data[i].length; j++) {
+                listaPersonaggi.push(data[i][j].name);
+            }
+            if (data[i][0]) {
+                listaFilm.push({
+                    film_id: data[i][0].filmIndex,
+                    film_titolo: data[i][0].filmName,
+                    personaggi_lista: listaPersonaggi
+                });
+            }
+        }
+
+        const csv1 = new csv(films);
+        const csv2 = new csv(listaFilm);
+        csv1.toDisk("./disneyData1.csv");
+        csv2.toDisk("./disneyData2.csv");
+        return console.log("Personaggi catalogati");
     })
     .catch(err => {
         console.log(err);
