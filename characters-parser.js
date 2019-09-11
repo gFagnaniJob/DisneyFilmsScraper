a4c2db4cbfdd6deeda82a13f024594383af931da
Giuseppe Fagnani <g.fagnani@reply.it>
2019-09-11 11:38:27 +0200

    suddivisi compiti su diversi file

---
+++ characters-parser.js
@@ -0,0 +1,18 @@
+const getPersonaggiData = async (html) => {
+    //     const personaggi = $("#Personaggi", innerHTML).next().next().find('li > a')
+//         .map((index, element) => {
+//             const name = element.children[0].data;
+//             return {
+//                 index: index + 1,
+//                 name
+//             }
+//         });
+
+//     const personaggiMap = $(".sinottico_testo_centrale > div > ul > li", innerHTML)
+//         .map((index, element) => {
+//             const name = element.children[0].data;
+//             console.log(name);
+//         });
+}
+
+module.exports = getPersonaggiData;
---
+++ film-parser.js
@@ -0,0 +1,48 @@
+const rp = require('request-promise');
+const $ = require('cheerio');
+
+const baseITAURL = 'https://it.wikipedia.org';
+const classiciITAURL = '/wiki/Classici_Disney';
+
+// const baseENURL = 'https://en.wikipedia.org';
+// const classiciENURL = '/wiki/List_of_Walt_Disney_Animation_Studios_films';
+
+const charactersParser = require('./characters-parser');
+
+
+
+const getFilmData = async () => {
+    const html = await rp(baseITAURL + classiciITAURL);
+
+    ($('#Elenco_completo', html).parent().next().find('tbody > tr').next().find('tr > td > i > a').length);
+    const filmMap = $('#Elenco_completo', html).parent().next().find('tbody > tr').next().find('tr > td > i > a')
+        .map(async (index, element) => {
+            const link = baseITAURL + element.attribs.href;
+            const name = element.children[0].data;
+
+            const innerHTML = await rp(link);
+            
+            charactersParser(innerHTML);
+
+
+            return {
+                index: index + 1,
+                name,
+                // personaggiMap,
+                link: link.indexOf(".php") > -1 ? null : link
+            }
+        })
+        .get();
+
+    return Promise.all(filmMap);
+}
+
+// getFilmData()
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
+
+module.exports = getFilmData;
